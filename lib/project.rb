2489b66a96cb1adbe9f7e432aac5a93c29fa008d
Tristan Brown <tgbrown@stanford.edu>
2018-10-02 11:47:35 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project
+  attr_accessor :title, :backers 
+
+
+  @@backer = []
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def self.backers
+    @@backers
+  end
+
+  def  add_backer(backer)
+    @backers << backer
+  end
+
+end
